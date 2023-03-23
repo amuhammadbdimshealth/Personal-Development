ac32d880aa9f1e809c316bd0be7cc5fcf8236f9d
AzureAD\ArifSultan <ams@stratocore.com>
2023-03-23 15:39:44 +0600

    explore - vue-select

---
+++ explore/vue-select/main.js
@@ -0,0 +1,31 @@
+export default {
+    props: {
+        options: {
+            type: Array,
+            required: true,
+        },
+        default: {
+            type: String,
+            required: false,
+            default: null,
+        },
+        tabindex: {
+            type: Number,
+            required: false,
+            default: 0,
+        },
+    },
+    data() {
+        return {
+            selected: this.default
+                ? this.default
+                : this.options.length > 0
+                    ? this.options[0]
+                    : null,
+            open: false,
+        };
+    },
+    mounted() {
+        this.$emit("input", this.selected);
+    },
+};
