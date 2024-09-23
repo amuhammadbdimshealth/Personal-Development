26e7da202306fb22283b51fe8295bc010f07c8d7
AzureAD\ArifSultan <ams@stratocore.com>
2024-09-23 17:51:46 +0600

    object basics

---
+++ odin-project/07-object-basics/js.js
@@ -0,0 +1,31 @@
+/*
+As an example of what we mean, let’s consider a sumOfTripledEvens function. It will:
+
+Take in an array.
+For every even number, it will triple it.
+Then it will sum all those even numbers.
+*/
+
+const sumOfTripledEvens = (list) => {
+    let sum = 0;
+    list.forEach(el => {
+        const isEven = (el % 2 == 0);
+        sum = isEven ? (el * 3) + sum : sum;
+    });
+    return sum;
+} 
+
+
+const sumOfTripledEvens2 = (list = []) => {
+    return list
+        .filter(el => el % 2 == 0)
+        .map(el => el * 3)
+        .reduce((total, el) => {        
+            return total + el
+        }, 0)
+} 
+
+const result = sumOfTripledEvens([1,2,3,4,5,6,7,8])
+const result2 = sumOfTripledEvens2([1,2,3,4,5,6,7,8])
+result
+result2
