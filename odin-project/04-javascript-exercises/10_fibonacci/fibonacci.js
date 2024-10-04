4a073be02cd229becc0702334419200d7394cc14
AzureAD\ArifSultan <ams@stratocore.com>
2024-10-04 18:04:10 +0600

    fibonacci

--- odin-project/04-javascript-exercises/10_fibonacci/fibonacci.js
+++ odin-project/04-javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {        
+    if (num <= 0) return "OOPS";
+    if(num <= 2) {
+        return 1;
+    } 
+    else {
+        num = num - 1        
+        const curr = fibonacci(num);
+        const prev = fibonacci(num-1);        
+        return prev + curr;
+    }
 };
+// const result = fibonacci(6);
+// result
 
 // Do not edit below this line
 module.exports = fibonacci;
