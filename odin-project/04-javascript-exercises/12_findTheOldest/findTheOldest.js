e5b4d4116b79c9e6201afc8d35db0ac1e246c0c7
AzureAD\ArifSultan <ams@stratocore.com>
2024-10-14 16:02:44 +0600

    exercise 12-findTheOldest

--- odin-project/04-javascript-exercises/12_findTheOldest/findTheOldest.js
+++ odin-project/04-javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.map(man => {
+        if (!('yearOfDeath' in man)) {
+            man['yearOfDeath'] = (new Date()).getFullYear();
+        }
+        return man;
+    })
+        .reduce((oldest, man) => {
+            const ageOldest = oldest.yearOfDeath - oldest.yearOfBirth;
+            const ageMan = man.yearOfDeath - man.yearOfBirth;
+            if (ageMan > ageOldest) return man;
+            else return oldest;
+        });
 };
+
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 1066,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+];
+
+const oldest = findTheOldest(people);
+oldest;
 
 // Do not edit below this line
 module.exports = findTheOldest;
